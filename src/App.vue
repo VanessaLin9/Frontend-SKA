198deed4602c0cd7944b2bb64eff206e34db30ab
erik.wu36 <erik.wu@titansoft.com.sg>
2023-09-05 11:40:54 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import OtpVerify from '@/components/OtpVerify.vue'
+import Profile from '@/components/Profile.vue'
+</script>
+
+<template>
+  <Profile/>
+  <OtpVerify/>
+</template>
---
+++ src/components/OtpVerify.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <form>
+      <div class="otp-container">
+        <h3 class="otp-title">Enter verification</h3>
+        <div class="input-group">
+          <input class="input">
+          <input class="input">
+          <input class="input">
+          <input class="input">
+        </div>
+      </div>
+    </form>
+</template>
+<style scoped>
+.otp-container {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+}
+.otp-title {
+  font-size: 2rem;
+}
+
+.input-group {
+  display: flex;
+  gap: 1rem;
+}
+
+.input-group > .input {
+  width: 8rem;
+  height: 12rem;
+  text-align: center;
+  font-size: 2rem;
+}
+</style>
