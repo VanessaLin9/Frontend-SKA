62886d9376699dcc800a9a555abd876f0bc53eb5
VanessaLin9 <vanessa7591@gmail.com>
2024-01-17 22:45:24 +0800

    feat: authorizaion

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,39 @@ import { ref } from 'vue'
 
 const isLoading = ref(false);
 const token = ref('');
+const user = { profile: {
+  username: '',
+  quote:'',
+  photo:''
+}};
 
 const handleLoading = () => {
   isLoading.value = !isLoading.value
 }
+
+const authorization = async () => {
+  try {
+    const response = await fetch('/api/auth', {
+      headers: {
+        'Authorization': token.value
+      }
+    });
+    const data = await response.json();
+    if(data){
+      user.profile = data;
+      console.log(user.profile);
+    } else {
+      throw new Error('Error: authorization failed');
+    }
+  } catch (error) {
+    console.error(error)
+  }
+};
+
 const handleToken = (value: string) => {
   token.value = value; 
   console.log(token.value);
+  authorization()
 }
 </script>
 
